9504eee1db6ee38f31505237b5f90cc0c1695944
psheno <emv2298@icloud.com>
2024-02-17 15:04:44 +0300

    last online

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,21 +2,43 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\UnauthorizedException;
 
 class Authenticate extends Middleware
 {
     /**
-     * Get the path the user should be redirected to when they are not authenticated.
+     * Determine if the user is logged in to any of the given guards.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
      */
-    protected function redirectTo(Request $request): ?string
+    protected function authenticate($request, array $guards)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                $user = User::findOrFail(Auth::id());
+                $user->updated_at = Carbon::now();
+                $user->save();
+                return $this->auth->shouldUse($guard);
+            }
+        }
+
+        $this->unauthenticated($request, $guards);
     }
 
-        /**
+    /**
      * Handle an unauthenticated user.
      *
      * @param  \Illuminate\Http\Request  $request
